ee4bb0d79fcb8554737009000cee4c29e4503e4d
Pfefan <stefanpfeiffer04@gmail.com>
2023-03-05 21:09:54 +0100

    list files done

--- handlers/list_files.py
+++ handlers/list_files.py
@@ -1,19 +1,19 @@
-import asyncio
-
 import discord
 from discord.ext import commands
+from discord.ui import Button, View
 
 from handlers.database_handler import FileManager
 from logging_formatter import ConfigLogger
 
 
-class FileList_Service(discord.ui.View):
+class FileList_Service():
     def __init__(self, bot: commands.Bot) -> None:
         self.db_handler = FileManager()
         self.logger = ConfigLogger().setup()
         self.bot = bot
+        self.page = 1
 
-    async def get_embed(self, page=1):
+    async def get_embed(self, page, msg_interaction):
         files = self.db_handler.get_files()
         chunks = [files[i:i+8] for i in range(0, len(files), 8)]
         num_pages = len(chunks)
@@ -28,44 +28,46 @@ class FileList_Service(discord.ui.View):
             user = await self.bot.fetch_user(file.user_id)
             channel = self.bot.get_channel(file.channel_id)
             embed.add_field(
-                name=f"{file.id}). {file.file_name}.{file.file_type}", 
+                name=f"{file.id}). {file.file_name}.{file.file_type}",
                 value=f"Uploader: {user.mention} | Size: {file.file_size} \n Saved in channel: {channel.name}",
                 inline=False
             )
 
         if num_pages > 1:
             embed.set_footer(text=f"Page {page}/{num_pages}")
-            return embed, ButtonView(page=page, num_pages=num_pages)
+
+            previous_btn = Button(label="Previous", style=discord.ButtonStyle.primary, emoji="⬅️", disabled=(page == 1))
+            next_btn = Button(label="Next", style=discord.ButtonStyle.primary, emoji="➡️", disabled=(page == num_pages))
+
+            async def previous_callback(interaction, page):
+                if page > 1:
+                    new_page = page - 1
+                    embed, view = await self.get_embed(new_page, msg_interaction)
+                    await interaction.response.defer()
+                    await msg_interaction.edit_original_response(embed=embed, view=view)
+            
+            async def next_callback(interaction, page, num_pages):
+                if page < num_pages:
+                    new_page = page + 1
+                    embed, view = await self.get_embed(new_page, msg_interaction)
+                    await interaction.response.defer()
+                    await msg_interaction.edit_original_response(embed=embed, view=view)
+
+            previous_btn.callback = lambda i: previous_callback(i, page)
+            next_btn.callback = lambda i: next_callback(i, page, num_pages)
+
+            view = View()
+            view.add_item(previous_btn)
+            view.add_item(next_btn)
+
+            return embed, view
         else:
             return embed, None
 
+
     async def embed(self, interaction, page=1):
-        embed, view = await self.get_embed(page)
+        embed, view = await self.get_embed(page, interaction)
         await interaction.response.send_message(embed=embed, view=view)
 
     async def main(self, interaction):
         await self.embed(interaction)
-
-
-class ButtonView(discord.ui.View):
-    def __init__(self, page, num_pages):
-        super().__init__()
-        self.page = page
-        self.num_pages = num_pages
-
-        if page < num_pages:
-                @discord.ui.button(label="Next", style=discord.ButtonStyle.primary, emoji="➡️")
-                async def next_callback(self, button, interaction):
-                    print("next")
-        if page > 1:
-            @discord.ui.button(label="Previous", style=discord.ButtonStyle.primary, emoji="⬅️")
-            async def previous_callback(self, button, interaction):
-                print("previous")
-
-    async def on_button_click(self, interaction: discord.Interaction, button: discord.ui.Button,):
-        if button.custom_id == "next_button":
-            self.page += 1
-        elif button.custom_id == "previous_button":
-            self.page -= 1
-        embed, view = await FileList_Service().get_embed(self.page)
-        await interaction.response.edit_message(embed=embed, view=view)

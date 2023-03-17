3305dbeaac184df683d43231bd75f851f846352c
Pfefan <stefanpfeiffer04@gmail.com>
2023-03-17 13:05:33 +0100

    pylint test fix

--- tests/test_000_databasemodel.py
+++ tests/test_000_databasemodel.py
@@ -1,3 +1,8 @@
+# pylint: disable=redefined-outer-name
+"""
+This module contains test cases for database model.
+"""
+
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -6,29 +11,38 @@ from local_db.saved_files import Base, SavedFile
 
 
 @pytest.fixture(scope="module")
-def db():
+def test_db():
+    """
+    Create an in-memory SQLite database and a session to interact with it.
+    """
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
     yield session
     session.close()
     Base.metadata.drop_all(engine)
 
 
-def test_create_saved_file(db):
+def test_create_saved_file(test_db):
+    """
+    Test the creation of a saved file in the database.
+    """
     file = SavedFile(user_id=1, channel_id=1, file_name="file1", file_size="100MB", file_type="txt")
-    db.add(file)
-    db.commit()
+    test_db.add(file)
+    test_db.commit()
     assert file.id is not None
 
 
-def test_retrieve_saved_file(db):
+def test_retrieve_saved_file(test_db):
+    """
+    Test the retrieval of a saved file from the database.
+    """
     file = SavedFile(user_id=1, channel_id=1, file_name="file1", file_size="100MB", file_type="txt")
-    db.add(file)
-    db.commit()
+    test_db.add(file)
+    test_db.commit()
 
-    retrieved_file = db.query(SavedFile).filter_by(id=file.id).first()
+    retrieved_file = test_db.query(SavedFile).filter_by(id=file.id).first()
     assert retrieved_file.id == file.id
     assert retrieved_file.user_id == file.user_id
     assert retrieved_file.channel_id == file.channel_id
@@ -37,24 +51,30 @@ def test_retrieve_saved_file(db):
     assert retrieved_file.file_type == file.file_type
 
 
-def test_update_saved_file(db):
+def test_update_saved_file(test_db):
+    """
+    Test the update of a saved file in the database.
+    """
     file = SavedFile(user_id=1, channel_id=1, file_name="file1", file_size="100MB", file_type="txt")
-    db.add(file)
-    db.commit()
+    test_db.add(file)
+    test_db.commit()
 
     file.file_name = "new_file_name"
-    db.commit()
+    test_db.commit()
 
-    retrieved_file = db.query(SavedFile).filter_by(id=file.id).first()
+    retrieved_file = test_db.query(SavedFile).filter_by(id=file.id).first()
     assert retrieved_file.file_name == "new_file_name"
 
 
-def test_delete_saved_file(db):
+def test_delete_saved_file(test_db):
+    """
+    Test the deletion of a saved file from the database.
+    """
     file = SavedFile(user_id=1, channel_id=1, file_name="file1", file_size="100MB", file_type="txt")
-    db.add(file)
-    db.commit()
+    test_db.add(file)
+    test_db.commit()
 
-    db.delete(file)
-    db.commit()
+    test_db.delete(file)
+    test_db.commit()
 
-    assert db.query(SavedFile).filter_by(id=file.id).first() is None
+    assert test_db.query(SavedFile).filter_by(id=file.id).first() is None
--- tests/test_001_database_handler.py
+++ tests/test_001_database_handler.py
@@ -1,3 +1,4 @@
+# pylint: disable=redefined-outer-name
 import pytest
 from handlers.database_handler import LocalDBManager
 
